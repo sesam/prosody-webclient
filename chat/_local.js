0cbfc262bff9e879f137da5dfb9d02a8c59b5c66
Simon B. <simon.bohlin@gmail.com>
2010-11-15 20:54:40 +0100

    add more support for localization

--- chat/_local.js
+++ chat/_local.js
@@ -1,14 +1,15 @@
 document.title = 'Piratpartiets Webbchat';
 
-window.room = "test@conference.prosody.im";
-window.bosh = "/bosh";
+window.room = "piratpartiet@chat.piratpartiet.se";
+window.bosh = "/http-bind";
 
 
-var muc_local_adjustments = function()
+var muc_login_local_adjustments = function()
 	{
 	    document.getElementById('what-name').innerHTML = 'Vilket namn vill du anv&auml;nda i chatten?';
 	    document.getElementById('name-colon').innerHTML = 'Namn:';
 	    document.getElementById('join').value = 'G\u00e5 in';
+	    window.muc_placeholder = 'skriv ditt meddelande här och tryck enter';
 	}
 
-addLoader("load", muc_local_adjustments);
+addLoader("load", muc_login_local_adjustments);
