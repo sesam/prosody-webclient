2c1d8169fa2ee3aceb7b4eb870da23d1fbf9c7e1
Simon B. <simon.bohlin@gmail.com>
2010-11-15 21:26:41 +0100

    switch encoding to utf-8

--- chat/_local.js
+++ chat/_local.js
@@ -9,7 +9,7 @@ var muc_login_local_adjustments = function()
 	    document.getElementById('what-name').innerHTML = 'Vilket namn vill du anv&auml;nda i chatten?';
 	    document.getElementById('name-colon').innerHTML = 'Namn:';
 	    document.getElementById('join').value = 'G\u00e5 in';
-	    window.muc_placeholder = 'skriv ditt meddelande h‰r och tryck enter';
+	    window.muc_placeholder = 'skriv ditt meddelande h√§r och tryck enter';
 	}
 
 addLoader("load", muc_login_local_adjustments);
