51d90350ff5a76dd2bf9cc291e72d3a992220fe0
Simon B. <simon.bohlin@gmail.com>
2010-11-15 21:23:44 +0100

    make XMPP server configurable. Group globals withing prefix xmpp_*

--- chat/_local.js
+++ chat/_local.js
@@ -1,15 +1,15 @@
 document.title = 'Piratpartiets Webbchat';
 
-window.room = "piratpartiet@chat.piratpartiet.se";
-window.bosh = "/http-bind";
-
+window.xmpp_room = "piratpartiet@chat.piratpartiet.se";
+window.xmpp_bosh = "/http-bind";
+window.xmpp_srv  = "anon.piratechat.net";
 
 var muc_login_local_adjustments = function()
 	{
 	    document.getElementById('what-name').innerHTML = 'Vilket namn vill du anv&auml;nda i chatten?';
 	    document.getElementById('name-colon').innerHTML = 'Namn:';
 	    document.getElementById('join').value = 'G\u00e5 in';
-	    window.muc_placeholder = 'skriv ditt meddelande h√§r och tryck enter';
+	    window.muc_placeholder = 'skriv ditt meddelande h‰r och tryck enter';
 	}
 
 addLoader("load", muc_login_local_adjustments);
